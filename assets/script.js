ac47fa9f6a8ffb0d5d9e61f863228f90bb8795b2
gwenparadis <gwendelynparadis2@gmail.com>
2022-10-16 15:46:58 -0400

    js improvements--code working properly except for running the code twice and concat-ing the two passwords together in to one

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,69 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+
+//necessary variables
+let passwordLength;
+let lowercase;
+let uppercase;
+let numbers;
+let specials;
+
+// Password characters selection
+const lower = ["abcdefghijklmnopqrstuvwxyz"];
+const upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+const num = ["1234567890"]
+const special = ["~!@#$%^&*()_+"];
+let passwordChar = [];
+let password = "";
+
+//TO DO: Generate Passwork function with Acceptance Criteria
+
+function generatePassword() {
+  // First prompt asking for users password length
+  let passwordLength = prompt("Choose the length of your password. This needs to be between 8 - 128");
+  if (!passwordLength || passwordLength <= 7 || passwordLength >= 129) {
+    return alert("Choose a number between 8-128.");
+  }
+
+  // remaining prompts
+  let lowercase = confirm("Click ok if would you like your password to contain lowercase letters.");
+  if (lowercase === true) {
+    passwordChar += lower;
+  }
+  uppercase = confirm("Click ok if would you like your password to contain uppercase letters.");
+  if (uppercase === true) {
+    passwordChar = passwordChar.concat(upper);
+  }
+  numbers = confirm("Click ok if would you like your password to contain numbers.");
+  if (numbers === true) {
+    passwordChar = passwordChar.concat(num);
+  }
+  specials = confirm("Click ok if would you like your password to contain special characters.");
+  if (specials === true) {
+    passwordChar = passwordChar.concat(special);
+  }
+
+  console.log("passwordChar", passwordChar)
+
+  // select random characters from the user input
+  for (let i = 0; i < passwordLength; i++) {
+    password += passwordChar[Math.floor(Math.random() * passwordChar.length)]
+  }
+
+  return (password);
+}
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () {
+  generatePassword();
+  writePassword();
+});
